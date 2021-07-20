f676fabc5933969042490b8d9456c0c063c474f3
Muthukumar16071999 <muthukumar.maniyan99@gmail.com>
2021-07-20 20:51:21 +0530

    Completed

---
+++ src/org/rat/Basics.java
@@ -0,0 +1,12 @@
+package org.rat;
+
+public class Basics {
+	public static void main(String[] args) {
+		
+		System.out.println("Muthukumar");
+		System.out.println("hi guys....");
+		
+		System.out.println("Peaceful life.........");
+	}
+
+}
